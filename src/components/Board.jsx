678d14853748f450c7f47e77df87a3d2f36ceb57
Nicole Orchard <norchard24@gmail.com>
2017-08-09 17:27:25 -0700

    Board rendering. Cells onclick show numbers

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,40 +1,80 @@
 import React, { Component } from 'react'
 
+// class Cell extends Component {
+//   constructor(props) {
+//     super(props);
+//     console.log(props)
+//     this.state = {
+//       guessed: props.guessed,
+//       value: props.value,
+//       onclick: props.onclick
+//     };
+//   }
+//
+//   componentWillReceiveProps(nextProps) {
+//     if(this.props != nextProps){
+//       console.log("Updating cell state!")
+//       this.state = {};
+//     }
+//   }
+//
+//   render() {
+//     console.log("rendering Cell")
+//     if ( this.state.guessed ){
+//       return <div className="cell">{ this.state.value }</div>
+//     } else {
+//       return (<div
+//                 className="cell"
+//                 onClick={ this.state.onclick }>
+//               </div>)
+//     }
+//   }
+// }
+
 class Board extends Component {
   constructor(props) {
     super(props);
     console.log("Board props: ", props);
-    this.state = {size: props.size};
+    this.state = {
+      dimension: props.dimension,
+      size: props.size.toLowerCase(),
+      board: [],
+      gameOver: false
+    };
   }
 
   componentWillReceiveProps(nextProps) {
-    if(this.props != nextProps)
-      this.state = {size: nextProps.size};
+    if(this.props != nextProps){
+      this.state = { dimension: nextProps.dimension, size: nextProps.size.toLowerCase() };
+      this.createBoard(nextProps.dimension);
+    }
   }
 
-  createBoard(size){
-    this.state.board = new Array(size).fill(undefined).map(function(){return new Array(size).fill(undefined)})
-    this.placeMines(size)
+  createBoard(dimension){
+    this.state.board = new Array(dimension).fill(undefined)
+                       .map(function(){return new Array(dimension).fill(undefined)})
+    this.state.guessed = new Array(dimension).fill(undefined)
+                         .map(function(){return new Array(dimension).fill(0)})
+    this.placeMines(dimension - 1)
     this.fillBoard()
     console.log("creating board: ", this.state.board)
+    console.log("creating guessed: ", this.state.guessed)
   }
 
   placeMines(mines){
-    console.log("mines: ", mines)
-    for (var i = 1; i < mines; i++){
+    for (var i = 0; i < mines; i++){
       do {
         var row = Math.floor(Math.random() * mines)
         var col = Math.floor(Math.random() * mines)
       } while (this.state.board[row][col] != undefined)
       this.state.board[row][col] = -1
-      // console.log(this.state.board)
     }
   }
 
   fillBoard() {
-    var size = this.state.size
-    for (var row = 0; row < size; row++){
-      for (var col = 0; col < size; col++){
+    var dimension = this.state.dimension
+    for (var row = 0; row < dimension; row++){
+      for (var col = 0; col < dimension; col++){
         if (this.state.board[row][col] == undefined)
           this.state.board[row][col] = this.countNeighboringMines(row,col)
       }
@@ -47,8 +87,8 @@ class Board extends Component {
       for (var offsetCol = -1; offsetCol <= 1; offsetCol++){
         var compareRow = row + offsetRow
         var compareCol = col + offsetCol
-        if (compareRow >= 0 && compareRow < this.state.size &&
-            compareCol >= 0 && compareCol < this.state.size &&
+        if (compareRow >= 0 && compareRow < this.state.dimension &&
+            compareCol >= 0 && compareCol < this.state.dimension &&
             this.state.board[compareRow][compareCol] == -1)
           count += 1
       }
@@ -56,14 +96,74 @@ class Board extends Component {
     return count
   }
 
+  guessNeighboringZeros(row, col) {
+    var offSet = [-1, 0, 1], rowOffSet, colOffset
+    for ( rowOffSet in offSet ){
+      for ( colOffset in offSet ){
+        var compareRow = row + rowOffSet
+        var compareCol = col + colOffset
+        if (compareRow >= 0 && compareRow < this.state.dimension &&
+            compareCol >= 0 && compareCol < this.state.dimension){
+          this.state.guessed[compareRow][compareCol] = 1
+          if (this.state.board[compareRow][compareCol] == 0)
+            this.guessNeighboringZeros(compareRow, compareCol)
+        }
+      }
+    }
+  }
+  /* TODO: refactor all functions to use setState and be 'pure' */
+
+  markGuessed(row, col) {
+    var newGuessed = this.state.guessed.slice()
+    newGuessed[row][col] = 1
+    this.setState({ guessed: newGuessed })
+  }
+
+  gameOver(){
+    var newGuessed = this.state.guessed.slice()
+    for (var row in newGuessed){
+      newGuessed[row].fill(1)
+    }
+    this.setState({ guessed: newGuessed, gameOver: true })
+  }
+
+  clickHandler(e, value, row, col) {
+    this.markGuessed(row, col)
+    if (value == 0)
+      this.guessNeighboringZeros(row, col)
+    else if (value == -1) {
+      this.gameOver()
+    }
+    // console.log("Setting cell guessed to one")
+
+    // console.log("event: ", e.target)
+    // console.log("row index: ", rowIndex)
+    // console.log("col index: ", colIndex)
+    // console.log("guessed: ", this.state.guessed)
+  }
+
   render() {
-    // console.log("Board: this.state.size", this.state.size)
-    if (this.state.size != undefined)
-      this.createBoard(this.state.size)
+    console.log("rendering!")
     return(
-      <h1>Hi!</h1>
+      <div className={ "board " + this.state.size }>
+        {
+          this.state.board.map( (row, rowIndex) => {
+            return row.map( (cell, colIndex) => {
+              if ( this.state.guessed[rowIndex][colIndex] )
+                return <div className="cell">{ cell == -1 ? "*" : cell }</div>
+              else
+                return <div className="cell" onClick={ (e) => this.clickHandler(e, cell, rowIndex, colIndex) }></div>
+            })
+          })
+        }
+      </div>
     )
   }
 }
 
 export default Board
+
+// return <Cell
+//         guessed={ this.state.guessed[rowIndex][colIndex] }
+//         value={ cell }
+//         onclick={ (e) => this.clickHandler(e, cell, rowIndex, colIndex)} />
--- src/components/Minesweeper.jsx
+++ src/components/Minesweeper.jsx
@@ -4,38 +4,48 @@ import Board from "./board.jsx"
 class Minesweeper extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {size: undefined};
+    this.state = {dimension: undefined, size: "None"};
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(e) {
-    this.setState({ size: parseInt(e.target.value) })
-    console.log("Game: e.target.value: ", e.target.value)
+    var dimension;
+    switch(e.target.value){
+      case "Small":
+        dimension = 3
+        break
+      case "Medium":
+        dimension = 6
+        break
+      case "Large":
+        dimension = 9
+    }
+    this.setState({ size: e.target.value, dimension: dimension})
+    // console.log("Game: e.target.value: ", e.target.value)
     // this.state.size = parseInt(e.target.value)
-    console.log("Game: this.state.size: ", this.state.size)
+    // console.log("Game: this.state.size: ", this.state.size)
+    // console.log("Game: this.state.dimension: ", this.state.dimension)
   }
 
   render() {
     return(
       <div>
         {/* style={{border: 'solid crimson 1px', width:'208px', height: '208px', margin:'auto'}}> */}
-          <button
-            value = "3"
-            onClick={ this.handleChange }>
-            Small
-          </button>
-          <button
-            value = "6"
-            onClick={ this.handleChange }>
-            Medium
-          </button>
-          <button
-            value = "9"
-            onClick={ this.handleChange }>
-            Large
-          </button>
+          <input
+            type='button'
+            value='Small'
+            onClick={ this.handleChange } />
+          <input
+            type='button'
+            value='Medium'
+            onClick={ this.handleChange } />
+          <input
+            type='button'
+            value='Large'
+            onClick={ this.handleChange } />
           <Board
-            size={ this.state.size } />
+            dimension={ this.state.dimension }
+            size={ this.state.size }/>
       </div>
     )
   }
